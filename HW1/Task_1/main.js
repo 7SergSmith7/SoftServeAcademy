d890083010e5ec5c89c96874a8a41956ad36c1ba
7SergSmith7 <serg.kuznetsov89@gmail.com>
2021-04-14 20:41:38 +0300

    feat:HW1 - Task 1

---
+++ HW1/Task_1/main.js
@@ -0,0 +1,39 @@
+// Task 1
+let officeSize = [0, 0, 0];
+const oneCanCoating = 16;
+let numberСansForOffice;
+let welcomeMessage =
+  "Привет! Для вычисления количества необходимой красики, введите длину, ширину и высоту.";
+const finalAnswer = "Необхожимое количество банок для покраски стен - ";
+
+let wallArea;
+
+function getUserInput(number) {
+  do {
+    number = +prompt("Введите число от 1 до 1000.");
+    if (number > 0 && number <= 1000) break;
+  } while (true);
+  return number;
+}
+
+function calculateWallArea(arraySize) {
+  wallArea = 2 * arraySize[0] * arraySize[2] + 2 * arraySize[1] * arraySize[2];
+  return wallArea;
+}
+
+function calculateNumberСansForOffice(area) {
+  numberСansForOffice = Math.ceil(area / oneCanCoating);
+}
+
+function printAnswer(answer) {
+  alert(answer + numberСansForOffice);
+}
+
+function init() {
+  alert(welcomeMessage);
+  officeSize = officeSize.map((element) => getUserInput(element));
+  calculateNumberСansForOffice(calculateWallArea(officeSize));
+  printAnswer(finalAnswer);
+}
+
+init();
