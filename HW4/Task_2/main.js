e9414f237f26edd364e6b9d6e39487133da4ddd3
7SergSmith7 <serg.kuznetsov89@gmail.com>
2021-04-19 23:29:38 +0300

    feat:HW4 - Task 2

---
+++ HW4/Task_2/main.js
@@ -0,0 +1,67 @@
+// Task 2
+
+const alphabet = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
+};
+
+const morseString = ".... . -.--   .--- ..- -.. .";
+
+function getMorseWords(morse) {
+  return morse.split("   ");
+}
+
+function decodeMorse(morse) {
+  return getMorseWords(morse)
+    .map((word) =>
+      word
+        .split(" ")
+        .map((letеer) => alphabet[letеer])
+        .join("")
+    )
+    .join(" ");
+}
+
+function printAnswer(result) {
+  alert(result);
+}
+
+function init() {
+  printAnswer(decodeMorse(morseString));
+}
+
+init();
