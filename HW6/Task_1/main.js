9f057df2654335d574d11b000a58f29508711af6
7SergSmith7 <serg.kuznetsov89@gmail.com>
2021-04-23 02:34:16 +0300

    feat:HW6 - Task 1

---
+++ HW6/Task_1/main.js
@@ -0,0 +1,46 @@
+// Task 1
+
+class Hex {
+  constructor(value) {
+    this.value = value;
+  }
+
+  valueOf() {
+    return this.value;
+  }
+  convertToHex(value) {
+    return "0x" + value.toString(16).toUpperCase();
+  }
+
+  toJSON() {
+    return this.convertToHex(this.value);
+  }
+  toString() {
+    return this.convertToHex(this.value);
+  }
+  plus(hexObject) {
+    return new Hex(this.value + hexObject.valueOf());
+  }
+  minus(hexObject) {
+    return new Hex(this.value - hexObject.valueOf());
+  }
+  static parse(hexStr) {
+    return parseInt(hexStr.replace("0x", ""), 16);
+  }
+}
+
+const hex = new Hex(255);
+
+console.log(hex.toString());
+console.log(hex.valueOf());
+console.log(hex.toJSON());
+
+const hex2 = new Hex(1);
+
+console.log(hex.plus(hex2).toString());
+console.log(hex.plus(hex2).valueOf());
+console.log(hex.minus(hex2).toString());
+console.log(hex.minus(hex2).valueOf());
+
+console.log(Hex.parse("0xFF"));
+console.log(Hex.parse("FF"));
